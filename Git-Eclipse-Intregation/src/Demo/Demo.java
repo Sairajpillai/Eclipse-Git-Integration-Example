37d9c2daf8d1806072c30a038901ecd2aecf2f75
SairajPillai <Sairajpillai8@gmail.com>
2023-07-11 16:58:52 +0530

    Second Commit

--- Git-Eclipse-Intregation/src/Demo/Demo.java
+++ Git-Eclipse-Intregation/src/Demo/Demo.java
@@ -4,6 +4,8 @@ public class Demo {
 
 	public static void main(String[] args) {
 		System.out.println("This is a Demo project for integrating eclipse with Git");
+		
+		System.out.println("This is for updating the changes");
 
 	}
 
